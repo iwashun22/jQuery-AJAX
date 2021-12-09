068baa2cb2ebdc95e546876f30e3823141d96bcf
Shun Iwashita <iwashunabc519@gmail.com>
2021-12-09 10:40:05 +0700

    use delegate() method

--- web/script.js
+++ web/script.js
@@ -2,7 +2,6 @@ const removeIcon = `<i class="fas fa-trash"></i>`;
 
 $(function() {
    const $tableList = $('.list tbody');
-   const $form = $('#task-form');
    const $taskTitle = $('#task-title');
    const $taskStatus = $('#task-status');
    const $submit = $('#submit');
@@ -14,7 +13,6 @@ $(function() {
          $tableList.children().remove();
          $.each(tasks, (i, task) => {
             addToList($tableList, task);
-            addRemoveBtn();
          })
       },
       error: function() {
@@ -34,7 +32,6 @@ $(function() {
             data: task,
             success: function(newTask) {
                addToList($tableList, newTask);
-               addRemoveBtn();
             },
             error: function() {
                alert('Error sending data...');
@@ -49,6 +46,21 @@ $(function() {
       $taskTitle.val('');
       $taskStatus.val('');
    });
+
+   $tableList.delegate('.remove', 'click', function() {
+      const index = $(this).parent().parent().index();
+      $.ajax({
+         type: 'DELETE',
+         url: `/tasks-api/${index}`,
+         success: function() {
+            $(`.list tbody tr`).eq(index).remove();
+            console.log('Deleted task');
+         },
+         error: function() {
+            alert('Can not find data to delete or something went wrong...');
+         }
+      });
+   })
 })
 
 /**
@@ -64,23 +76,4 @@ function addToList(table, task) {
          <td><button class="remove">${removeIcon}</button></td>
       </tr>
    `);
-}
-
-function addRemoveBtn() {
-   const $btn = $('button.remove');
-   $btn.off('click');
-   $btn.on('click', function() {
-      const index = $(this).parent().parent().index();
-      $.ajax({
-         type: 'DELETE',
-         url: `/tasks-api/${index}`,
-         success: function() {
-            $(`.list tbody tr`).eq(index).remove();
-            console.log('Deleted task');
-         },
-         error: function() {
-            alert('Can not find data to delete or something went wrong...');
-         }
-      });
-   });
 }
